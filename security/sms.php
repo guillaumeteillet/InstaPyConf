bc873993091763303abd3a3f28e812220235adda
Guillaume TEILLET <guillaume@teillet.eu>
2019-09-08 16:34:48 +0800

    Update sms.php

--- security/sms.php
+++ security/sms.php
@@ -9,16 +9,16 @@ $arr = array('text' => 'Use 603 218 to verify your Instagram account.');
 $sms = json_encode($arr);
 $sms_decode = json_decode($sms);
 $sms_content = $sms_decode->{'text'};
-$security_code = str_replace('Use ', '', $sms_content);
-$security_code = str_replace(' to verify your Instagram account.', '', $security_code);
-$security_code = str_replace(' ', '', $security_code);
+//$security_code = str_replace('Use ', '', $sms_content);
+//$security_code = str_replace(' to verify your Instagram account.', '', $security_code);
+//$security_code = str_replace(' ', '', $security_code);
+$security_code = $_GET['securitycode'];
 
 echo $security_code;
 
 $json_file = array('challenge' => array('security_code' => $security_code));
 
-unlink('/home/ubuntu/InstaPy/logs/guillaumeteillet/state.json');
-$fp2 = fopen("/home/ubuntu/InstaPy/logs/guillaumeteillet/state.json", "w+");
+$fp2 = fopen("/home/ubuntu/InstaPy/logs/guillaumeteillet/securitycode.json", "w+");
 fwrite($fp2, json_encode($json_file));
 fclose($fp2);
 ?>
